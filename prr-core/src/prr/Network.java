59443654af0698b9e555a85104eac559a239acdb
vugonz <goncalo.r.azevedo@tecnico.ulisboa.pt>
2022-10-11 01:25:39 +0100

    Change uilib .jar path and remove code that is not compiling

--- prr-core/src/prr/Network.java
+++ prr-core/src/prr/Network.java
@@ -14,57 +14,12 @@ public class Network implements Serializable {
 	/** Serial number for serialization. */
 	private static final long serialVersionUID = 202208091753L;
 
-	/**
-	 * ___________________________________________________________________Mapas e Listas__________________________________________________________________________________________
-	*/
-	private Map<String, Client> __clients = new TreeMap<String, Client>(String.CASE_INSENSITIVE_ORDER);
-
-
-        // FIXME define attributes
-        // FIXME define contructor(s)
-        // FIXME define methods
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  	// ---------------------------- _____metodos imports______ ------------------------------------------------  //
-
-
 	public void importRegisterClient(String id, String nome, int taxId) {
-		Client _client = new Client(id, nome, taxId);
-		_clients.put(id, _client);
 	}
 
 
 
-
-
 	public void registerFromFields(String[] _fields) throws UnrecognizedEntryException{
-		if (_fields[0].equals("CLIENT")){
-			int _taxID = Integer.parseInt(_fields[3]);
-			importRegisterClient(_fields[1], _fields[2], _taxID);
-		} else if(_fields[0].equals("BASIC")){
-
-
-		} else if(_fields[0].equals("FANCY")){
-
-		} else if(_fields[0].equals("FRIENDS")){
-
-		} else {
-			throw new UnrecognizedEntryException(_fields[0]); // Cria uma nova excecao que e apanhada no importFile
-		}
-
 	}
 
 
@@ -76,18 +31,6 @@ public class Network implements Serializable {
 	 * @throws IOException if there is an IO erro while processing the text file
 	 */
 	void importFile(String filename) throws UnrecognizedEntryException, IOException  {
-		BufferedReader reader = new BufferedReader(new FileReader(txtfile));
-		String linhaa;
-		while ((linhaa = reader.readLine()) != null) {
-			String[] fields = linha.split("\\|");
-			try {
-				registerFromFields(fields);
-			} catch (BadEntryException bee) {
-				System.err.printf("WARNING: data desconhecida %s\n", bee.getMessage());
-				bee.printStackTrace();
-			}
-		}
-		reader.close();
-        }
+    }
 }
 
